f9bc78fe7f69f41f7cbaf1991f38be0b6e7a4325
Ayush Ghagre <147733805+AyushGhagre-09@users.noreply.github.com>
2026-01-08 10:58:43 +0530

    Update ResultNode.jsx

--- client/src/components/ResultNode.jsx
+++ client/src/components/ResultNode.jsx
@@ -2,7 +2,7 @@ import { Handle, Position } from "@xyflow/react";
 
 export default function ResultNode({ data }) {
   return (
-    <div className="bg-white rounded-xl shadow-md border min-w-64 min-h-37">
+    <div className="bg-white rounded-xl shadow-md border min-w-64 max-w-90 min-h-37">
       <div className="px-4 py-2 font-semibold border-b text-gray-700">
         AI Response
       </div>
