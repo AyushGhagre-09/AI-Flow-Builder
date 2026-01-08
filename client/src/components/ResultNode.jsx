7a192f6cc51bdbe142896a2863ec30505a2040da
AyushGhagre-09 <ghagareayush2001@gmail.com>
2026-01-08 11:38:38 +0530

    package installation

--- client/src/components/ResultNode.jsx
+++ client/src/components/ResultNode.jsx
@@ -1,5 +1,5 @@
 import { Handle, Position } from "@xyflow/react";
-
+import Markdown from 'react-markdown'
 export default function ResultNode({ data }) {
   return (
     <div className="bg-white rounded-xl shadow-md border min-w-64 max-w-90 min-h-37">
@@ -8,7 +8,7 @@ export default function ResultNode({ data }) {
       </div>
 
       <div className="p-3 text-sm text-gray-800 min-h-[80px]">
-        {data.value || "Response will appear here..."}
+        <Markdown>{data.value || "Response will appear here..."}</Markdown>
       </div>
 
       <Handle type="target" position={Position.Left} />
